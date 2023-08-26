8138e033796d6a445155269e0b4d12b4280c3197
Victor Guerra <vguerra@gmail.com>
2023-08-26 16:59:26 +0200

    make `tutorial-opt` visible to the testing framework

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -42,28 +42,17 @@ llvm_config.use_default_substitutions()
 config.excludes = ["Inputs", "Examples", "CMakeLists.txt", "README.txt", "LICENSE.txt"]
 
 # test_exec_root: The root path where tests should be run.
-# config.test_exec_root = os.path.join(config.standalone_obj_root, "test")
-# config.standalone_tools_dir = os.path.join(config.standalone_obj_root, "bin")
-# config.standalone_libs_dir = os.path.join(config.standalone_obj_root, "lib")
-
-# config.substitutions.append(("%standalone_libs", config.standalone_libs_dir))
+config.test_exec_root = os.path.join(config.project_binary_dir, "test")
+config.project_tools_dir = os.path.join(config.project_binary_dir, "tools")
 
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
 
-# tool_dirs = [config.standalone_tools_dir, config.llvm_tools_dir]
-tool_dirs = [config.llvm_tools_dir]
+tool_dirs = [config.project_tools_dir, config.llvm_tools_dir]
 tools = [
     "mlir-opt",
     "mlir-cpu-runner",
+    "tutorial-opt"
 ]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
-
-# llvm_config.with_environment(
-#     "PYTHONPATH",
-#     [
-#         os.path.join(config.mlir_obj_dir, "python_packages", "standalone"),
-#     ],
-#     append_path=True,
-# )

ca3bf7ced0f3e20438c9481cbd3f34e6f270f2e5
Victor Guerra <vguerra@gmail.com>
2023-08-31 17:07:37 +0200

    Migrate full unroll passes to tablegen style

--- lib/Transform/Affine/AffineFullUnroll.h
+++ lib/Transform/Affine/AffineFullUnroll.h
@@ -1,38 +1,13 @@
 #ifndef LIB_TRANSFORM_AFFINE_AFFINEFULLUNROLL_H_
 #define LIB_TRANSFORM_AFFINE_AFFINEFULLUNROLL_H_
 
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace tutorial {
 
-class AffineFullUnrollPass
-    : public PassWrapper<AffineFullUnrollPass,
-                         OperationPass<mlir::func::FuncOp>> {
-private:
-  void runOnOperation() override;
-
-  StringRef getArgument() const final { return "affine-full-unroll"; }
-
-  StringRef getDescription() const final {
-    return "Fully unroll all affine loops";
-  }
-};
-
-class AffineFullUnrollPassAsPatternRewrite
-    : public PassWrapper<AffineFullUnrollPassAsPatternRewrite,
-                         OperationPass<mlir::func::FuncOp>> {
-private:
-  void runOnOperation() override;
-
-  StringRef getArgument() const final { return "affine-full-unroll-rewrite"; }
-
-  StringRef getDescription() const final {
-    return "Fully unroll all affine loops using pattern rewrite engine";
-  }
-};
+#define GEN_PASS_DECL_AFFINEFULLUNROLL
+#include "lib/Transform/Affine/Passes.h.inc"
 
 } // namespace tutorial
 } // namespace mlir
